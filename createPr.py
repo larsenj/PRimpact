57ee59e22541932e90e694e96dd6f0d6a25cfda0
larsenj <larsen.jf@gmail.com>
2017-12-17 12:08:59 -0800

    updated db creation script and server program

--- createPr.py
+++ createPr.py
@@ -5,8 +5,8 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print ("Create successfully")
 
+#id int AUTO_INCREMENT NOT NULL,
 conn.execute('''CREATE TABLE Entries (
-            id int NOT NULL,
             mac text NOT NULL,
             food bool,
             water bool,
@@ -17,4 +17,11 @@ conn.execute('''CREATE TABLE Entries (
 
 print ("Table created succccssfngurghotighotgb")
 
+conn.execute("INSERT INTO Entries (mac, food, water, power, lat, lon) \
+        VALUES('00:00:00:00:00:00', 0, 0, 0, 123.45, 678.90);")
+
+conn.execute("INSERT INTO Entries (mac, food, water, power, lat, lon) \
+        VALUES('00:00:00:00:00:01', 1, 1, 1, 987.6, 543.21);")
+
+conn.commit()
 conn.close()
--- server.py
+++ server.py
@@ -21,7 +21,35 @@ class GetAllPoints(Resource):
         return jsonify(result)
 
 
+class Entries(Resource):
+
+    ''' 
+    #Need to figure out conversion of lat-lon distances to km 
+    #get all entries within specific radius
+    def get(self, lat, lon):
+        conn = db_connect.connect()
+        query = conn.execute()
+        result = {'data':[dict(zip(tuple(query.keys()) ,i )) for i in query.cursor]}
+        return jsonify(result)
+    '''
+    #will need to figure out rough lat/lon conversions into 
+    def post(self):
+        conn = db_connect.connect()
+        print(request.json)
+        mac = request.json['mac']
+        food = request.json['food']
+        water = request.json['water']
+        power = request.json['power']
+        lat = request.json['lat']
+        lon = request.json['lon']
+        query = conn.execute("INSERT INTO Entries \
+        VALUES('{0}', '{1}','{2}','{3}','{4}','{5}');".format(mac, food,
+            water, power, lat, lon))
+        return{'status':'success'}
+
+
 api.add_resource(GetAllPoints, '/all')
+api.add_resource(Entries, '/Entries')
 
 
 if __name__ == "__main__":
